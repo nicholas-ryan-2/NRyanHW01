bd6a2553a07cc2fbc961f8ac0764203687c9cf0d
Nick <nickryan@nicks-air.bc.edu>
2017-01-23 01:39:30 -0500

    Initial Commit

---
+++ You are awesome v3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You are awesome v3
+//
+//  Created by Nick on 1/23/17.
+//  Copyright © 2017 Nick Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
